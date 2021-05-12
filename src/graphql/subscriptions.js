0f1274feda7cdbec9c7d00edc474436dcb980fc8
Michael Zaikin <michael.zaikin@gmail.com>
2021-05-12 16:18:32 +0300

    API added

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = `subscription OnCreateNote {
+  onCreateNote {
+    id
+    name
+    description
+    image
+  }
+}
+`;
+export const onUpdateNote = `subscription OnUpdateNote {
+  onUpdateNote {
+    id
+    name
+    description
+    image
+  }
+}
+`;
+export const onDeleteNote = `subscription OnDeleteNote {
+  onDeleteNote {
+    id
+    name
+    description
+    image
+  }
+}
+`;
